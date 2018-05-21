174992a3ea8b8337d7e54431d1edf0d6fb0b6afe
floris de jongh <f.dejongh@student.fontys.nl>
2018-05-21 13:58:16 +0200

    cas
    Het is gelukt om een username op te halen. Ook word er opnieuw een
    ticket opgehaald als deze ongeldig is.

--- CAS/cas.php
+++ CAS/cas.php
@@ -1,19 +1,18 @@
 <?php
+$uri_parts = explode('?', $_SERVER['REQUEST_URI'], 2);
+$ticketUrl = 'http://' . $_SERVER['HTTP_HOST'] . $uri_parts[0];
 if(!isset($_GET['ticket']) && empty($_GET['ticket'])){
-
-    header("Location:http://total-health.testing.edufra.me/cas/login?service=http://".$_SERVER["HTTP_HOST"] . $_SERVER['REQUEST_URI']);
+    
+    fetchNewTicket($ticketUrl);
 } else {
-    //echo "Ticket = ".$_GET['ticket'];
-   //header('Content-type: application/xml');
- //  header("Location:http://total-health.testing.edufra.me/cas/proxyValidate?ticket=".$_GET['ticket']."&service=http://localhost/Cas/cas.php");
-
    $ch = curl_init();
     $headers = array(
     'Accept: application/xml',
     'Content-Type: application/xml',
 
     );
-    curl_setopt($ch, CURLOPT_URL, "http://total-health.testing.edufra.me/cas/proxyValidate?ticket=".$_GET['ticket']);
+    $url = "http://total-health.testing.edufra.me/cas/proxyValidate.xml?service=".$ticketUrl."&ticket=" .$_GET['ticket'];
+    curl_setopt($ch, CURLOPT_URL,$url );
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($ch, CURLOPT_HEADER, 0);
 
@@ -21,11 +20,21 @@ if(!isset($_GET['ticket']) && empty($_GET['ticket'])){
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
     curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-
     $result = curl_exec($ch);
-    var_dump($result);
 
-    
+        if (strpos($result, '@') !== false) {
+            // split response into valid username
+            $result = explode('@',$result,2);
+            $result = $result[1]; 
+            var_dump($result);
+        }
+        else{
+            fetchNewTicket($ticketUrl);
+        }
+}
 
+function fetchNewTicket($ticketUrl)
+{
+    header("Location:http://total-health.testing.edufra.me/cas/login?service=" .$ticketUrl);
 }
 ?>
